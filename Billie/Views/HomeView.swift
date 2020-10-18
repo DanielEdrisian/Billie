9bff4008bf6b4010dc91ba7430ae0ba811b671bf
samrshi <samrshi@outlook.com>
2020-10-18 07:19:40 -0400

    Added search to tab

--- Billie/Views/HomeView.swift
+++ Billie/Views/HomeView.swift
@@ -8,68 +8,67 @@
 import SwiftUI
 
 struct HomeView: View {
-    
-    @ObservedObject var user: UserModel = UserModel.shared
-    @State private var songs = [SongModel](repeating: SongModel.example(), count: 4)
-    
-    var body: some View {
-        NavigationView {
-            ScrollView(.vertical) {
-                VStack(alignment: .leading, spacing: 8) {
-                    Text("Recents")
-                        .font(.title2)
-                        .bold()
-                    
-                    ForEach(user.songs, id: \.id) { song in
-                        NavigationLink(destination: SongDetailView(publisher: SpotifyPublisher.shared, song: song)) {
-                            SongItemView(song: song)
-                        }
-                        .buttonStyle(PlainButtonStyle())
-                    }
-                    
-                    Spacer()
-                }
-                .padding()
+  
+  @State private var songs = [SongModel](repeating: SongModel.example(), count: 4)
+  
+  var body: some View {
+    NavigationView {
+      ScrollView(.vertical) {
+        VStack(alignment: .leading, spacing: 8) {
+          Text("Recents")
+            .font(.title2)
+            .bold()
+          
+          ForEach(songs, id: \.id) { song in
+            NavigationLink(destination: SongDetailView(publisher: SpotifyPublisher.shared, song: song)) {
+              SongItemView(song: song)
             }
-            .navigationBarTitle("Home")
+            .buttonStyle(PlainButtonStyle())
+          }
+          
+          Spacer()
         }
+        .padding()
+      }
+      .navigationBarTitle("Home")
     }
+  }
 }
 
 struct HomeView_Previews: PreviewProvider {
-    static var previews: some View {
-        HomeView()
-    }
+  static var previews: some View {
+    HomeView()
+  }
 }
 
 struct SongItemView: View {
-    let song: SongModel
-    
-    var body: some View {
-        HStack {
-            Rectangle()
-                .fill(Color.black)
-                .frame(width: 45, height: 45)
-            
-            VStack(alignment: .leading) {
-                Text(song.name)
-                    .fontWeight(.semibold)
-                
-                Text(song.artist)
-                    .font(.callout)
-            }
-            
-            Spacer()
-            
-            VStack {
-                Text("\(song.notes.count)")
-                Text(song.notes.count > 1 ? "Notes" : "Note")
-                    .font(.caption)
-            }
-            .foregroundColor(.secondary)
-        }
-        .padding()
-        .background(Color(.secondarySystemBackground))
-        .cornerRadius(8)
+  let song: SongModel
+  
+  var body: some View {
+    HStack {
+      Rectangle()
+        .fill(Color.black)
+        .frame(width: 45, height: 45)
+      
+      VStack(alignment: .leading) {
+        Text(song.name)
+          .fontWeight(.semibold)
+        
+        Text(song.artist)
+          .font(.callout)
+      }
+      
+      Spacer()
+      
+      VStack {
+        Text("\(song.notes.count)")
+        Text(song.notes.count > 1 ? "Notes" : "Note")
+          .font(.caption)
+      }
+      .foregroundColor(.secondary)
     }
+    .padding()
+    .background(Color(.secondarySystemBackground))
+    .cornerRadius(8)
+  }
 }
